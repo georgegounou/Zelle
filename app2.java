0e770d23ac97de68234fd9d31d3ac9ec9a9f26c3
george gounou <georgegounou@gmail.com>
2022-02-20 01:52:07 +0000

    This is my second commit

---
+++ app2.java
@@ -0,0 +1,8 @@
+echo "what is your name"
+  read name 
+  echo your name is $name 
+
+  echo "Please enter your card"
+  echo "Please enter your pin"
+  read -s pin 
+  echo "Thanks you entered a valid pin"
